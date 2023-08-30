55bde2257728ce1decf5e61e40d18fe113d03a36
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-08-30 12:44:05 -0300

    Mudança para StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 
 void main() {
-  final app = App();
+  const app = App();
   runApp(app);
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class AppState extends State<App> {
+  int numeroImagens = 0;
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: const Text("MInhas Imagens!")),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add), onPressed: () => print("Batata")),
-      body: Image.network(
-          "https://media.tenor.com/EQdcclOwXSkAAAAC/spongebob-spongebob-meme.gif"),
+          child: const Icon(Icons.add), onPressed: () => setState(() {})),
+      body: Text(numeroImagens.toString()),
     ));
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    numeroImagens++;
+    super.setState(fn);
+  }
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() {
+    return AppState();
   }
 }
